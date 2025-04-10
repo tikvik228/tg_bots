8a71995e4f0f14eb59adaf74a434726592a81efe
eugennne <tikvik228@mail.ru>
2025-04-10 18:41:15 +0300

    ..

---
+++ main.py
@@ -0,0 +1,180 @@
+# Импортируем необходимые классы.
+import logging
+from telegram.ext import Application, MessageHandler, filters, CommandHandler, ConversationHandler
+from datetime import datetime
+
+BOT_TOKEN = '7659787190:AAGnP4w_IXOK3YS-Qdz-N7qVK2ZCbTyj3qo'
+# Запускаем логгирование
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
+)
+
+logger = logging.getLogger(__name__)
+
+
+# Определяем функцию-обработчик сообщений.
+# У неё два параметра, updater, принявший сообщение и контекст - дополнительная информация о сообщении.
+'''async def echo(update, context):
+    await update.message.reply_text(f"Я получил сообщение {update.message.text}")
+
+
+async def start(update, context):
+    await update.message.reply_text("Я эхо-бот. Я умею не только повторять, но и присылать дату и время.")
+
+
+async def help_command(update, context):
+    await update.message.reply_text("Я пока не умею помогать... Я только ваше эхо.")
+
+
+async def get_time(update, context):
+    await update.message.reply_text(f" текущее время: {datetime.now().strftime('%H:%M:%S')}")
+
+
+async def get_date(update, context):
+    await update.message.reply_text(f" текущая дата: {datetime.now().strftime('%d.%m.%y')}")
+
+
+
+TIMER = 5  # таймер на 5 секунд
+
+
+def remove_job_if_exists(name, context):
+    """Удаляем задачу по имени.
+    Возвращаем True если задача была успешно удалена."""
+    current_jobs = context.job_queue.get_jobs_by_name(name)
+    if not current_jobs:
+        return False
+    for job in current_jobs:
+        job.schedule_removal()
+    return True
+
+
+# Обычный обработчик, как и те, которыми мы пользовались раньше.
+async def set_timer(update, context):
+    """Добавляем задачу в очередь"""
+    chat_id = update.effective_message.chat_id
+    try:
+        timer = int(context.args[0])
+        if timer < 0:
+            await update.effective_message.reply_text("Извините, не умею возвращаться в прошлое..")
+            return
+
+        job_removed = remove_job_if_exists(str(chat_id), context)
+        context.job_queue.run_once(task, timer, chat_id=chat_id, name=str(chat_id), data=timer)
+
+        text = f'Вернусь через {timer} с.!'
+        if job_removed:
+            text += ' Старая задача удалена.'
+        await update.effective_message.reply_text(text)
+    except (ValueError, IndexError):
+        await update.effective_message.reply_text(f"Использование: /set <секунд>")
+
+async def task(context):
+    """Выводит сообщение"""
+    job = context.job
+    await context.bot.send_message(job.chat_id, text=f'КУКУ! {job.data} c. прошли!')
+
+
+async def unset(update, context):
+    """Удаляет задачу, если пользователь передумал"""
+    chat_id = update.message.chat_id
+    job_removed = remove_job_if_exists(str(chat_id), context)
+    text = 'Таймер отменен!' if job_removed else 'У вас нет активных таймеров'
+    await update.message.reply_text(text)'''
+
+async def start(update, context):
+    await update.message.reply_text(
+        "Привет. Пройдите небольшой опрос, пожалуйста!\n"
+        "Вы можете прервать опрос, послав команду /stop.\n"
+        "В каком городе вы живёте?")
+
+    # Число-ключ в словаре states —
+    # втором параметре ConversationHandler'а.
+    return 1
+    # Оно указывает, что дальше на сообщения от этого пользователя
+    # должен отвечать обработчик states[1].
+    # До этого момента обработчиков текстовых сообщений
+    # для этого пользователя не существовало,
+    # поэтому текстовые сообщения игнорировались.
+
+
+async def first_response(update, context):
+    # Это ответ на первый вопрос.
+    # Мы можем использовать его во втором вопросе.
+    print(context, update.message.text)
+    locality = update.message.text
+    await update.message.reply_text(
+        f"Какая погода в городе {locality}?")
+    # Следующее текстовое сообщение будет обработано
+    # обработчиком states[2]
+    return 2
+
+
+async def just_weather_response(update, context):
+    # Это ответ на первый вопрос.
+    # Мы можем использовать его во втором вопросе.
+    print(context, update.message.text)
+    locality = update.message.text
+    await update.message.reply_text(
+        f"Какая погода в городе {locality}?")
+    # Следующее текстовое сообщение будет обработано
+    # обработчиком states[2]
+    return 2
+
+
+async def second_response(update, context):
+    # Ответ на второй вопрос.
+    # Мы можем его сохранить в базе данных или переслать куда-либо.
+    weather = update.message.text
+    logger.info(weather)
+    await update.message.reply_text("Спасибо за участие в опросе! Всего доброго!")
+    return ConversationHandler.END  # Константа, означающая конец диалога.
+    # Все обработчики из states и fallbacks становятся неактивными.
+
+
+async def stop(update, context):
+    await update.message.reply_text("Всего доброго!")
+    return ConversationHandler.END
+
+def main():
+    # Создаём объект Application.
+    # Вместо слова "TOKEN" надо разместить полученный от @BotFather токен
+    application = Application.builder().token(BOT_TOKEN).build()
+    #text_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, echo)
+
+    conv_handler = ConversationHandler(
+        # Точка входа в диалог.
+        # В данном случае — команда /start. Она задаёт первый вопрос.
+        entry_points=[CommandHandler('start', start)],
+
+        # Состояние внутри диалога.
+        # Вариант с двумя обработчиками, фильтрующими текстовые сообщения.
+        states={
+            # Функция читает ответ на первый вопрос и задаёт второй.
+            1: [MessageHandler(filters.TEXT & ~filters.COMMAND, first_response)],
+            # Функция читает ответ на второй вопрос и завершает диалог.
+            2: [MessageHandler(filters.TEXT & ~filters.COMMAND, second_response)],
+            3: [MessageHandler(filters.TEXT & ~filters.COMMAND, just_weather_response)]
+        },
+
+        # Точка прерывания диалога. В данном случае — команда /stop.
+        fallbacks=[CommandHandler('stop', stop)]
+    )
+
+    application.add_handler(conv_handler)
+
+    # Регистрируем обработчик в приложении.
+    #application.add_handler(text_handler)
+    application.add_handler(CommandHandler("start", start))
+    '''application.add_handler(CommandHandler("help", help_command))
+    application.add_handler(CommandHandler("time", get_time))
+    application.add_handler(CommandHandler("date", get_date))
+    application.add_handler(CommandHandler("set", set_timer))
+    application.add_handler(CommandHandler("unset", unset))'''
+
+    # Запускаем приложение.
+    application.run_polling()
+
+# Запускаем функцию main() в случае запуска скрипта.
+if __name__ == '__main__':
+    main()
